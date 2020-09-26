6e40449fd519af5e65cf59718bf07c766094ebe7
Wenrui Zhang <zhangwenrui@tamu.edu>
2020-09-26 00:55:38 -0500

    final version

--- functions/TSSLBP.py
+++ functions/TSSLBP.py
@@ -126,6 +126,8 @@ class PSP_spike_long_time(torch.autograd.Function):  # a and u is the incremnet 
 
         mem = torch.zeros((shape[0], shape[1], shape[2], shape[3]), dtype=glv.dtype).to(glv.device)
         syn = torch.zeros((shape[0], shape[1], shape[2], shape[3]), dtype=glv.dtype).to(glv.device)
+        ref = torch.zeros((shape[0], shape[1], shape[2], shape[3], shape[4]), dtype=glv.dtype).to(glv.device)
+        delta_refs = torch.zeros((shape[0], shape[1], shape[2], shape[3], shape[4], shape[4]), dtype=glv.dtype).to(glv.device)
         mems = []
         mem_updates = []
         outputs = []
@@ -133,11 +135,20 @@ class PSP_spike_long_time(torch.autograd.Function):  # a and u is the incremnet 
         outputs = []
         for t in range(n_steps):
             mem_update = (-theta_m) * mem + inputs[..., t]
+            delta_ref = (-theta_m) * ref
             mem += mem_update
+            ref += delta_ref
 
             out = mem > threshold
             out = out.type(glv.dtype)
             mems.append(mem)
+            if t > 0:
+                out_tmp = out.unsqueeze(-1).repeat(1, 1, 1, 1, t)
+                ref[..., 0:t] *= (1-out_tmp)
+                delta_ref[..., 0:t] *= out_tmp
+            ref[..., t] = (-1) * mem * out
+            delta_refs[..., 0:t, t] = delta_ref[..., 0:t]
+
             mem = mem * (1-out)
             outputs.append(out)
             mem_updates.append(mem_update)
@@ -148,28 +159,32 @@ class PSP_spike_long_time(torch.autograd.Function):  # a and u is the incremnet 
         mem_updates = torch.stack(mem_updates, dim = 4)
         syns_posts = torch.stack(syns_posts, dim = 4)
         outputs = torch.stack(outputs, dim = 4)
-        ctx.save_for_backward(mem_updates, outputs, mems, torch.tensor([threshold]))
+        ctx.save_for_backward(mem_updates, outputs, mems, delta_refs, torch.tensor([threshold]))
         return syns_posts
 
     @staticmethod
     def backward(ctx, grad_delta):
-        (delta_u, outputs, u, others) = ctx.saved_tensors
+        (delta_u, outputs, u, delta_refs, others) = ctx.saved_tensors
         start_time = time()
         shape = outputs.shape
         n_steps = glv.n_steps
         threshold = others[0].item()
 
-        # part two, intra-neuron: effect of reset
-        # partial_u_extend = partial_u.unsqueeze(-1).repeat(1, 1, 1, 1, 1, n_steps)
-        # partial_a_2 = refs * partial_u_extend.transpose(4, 5)
-        # partial_a_2 = torch.einsum('...ij, ...jk -> ...ik', partial_a_2, partial_a)
-        partial_a_tmp = glv.partial_a[..., 0, :].repeat(shape[0], shape[1], shape[2], shape[3], 1)
         grad_a = torch.empty_like(delta_u)
+        
         for t in range(n_steps):
-            grad_a[..., t] = torch.sum(partial_a_tmp[..., 0:n_steps-t]*grad_delta[..., t:n_steps], dim=4) 
+            # part one, inter-neuron
+            partial_a_inter = glv.partial_a[..., t, :].repeat(shape[0], shape[1], shape[2], shape[3], 1)
+
+            # part two, intra-neuron: effect of reset
+            partial_a_intra = torch.einsum('...j, ...jk -> ...k', partial_a_inter, delta_refs)
+
+            # part one, inter-neuron + part two, intra-neuron
+            partial_a_all = partial_a_inter + partial_a_intra
+
+            grad_a[..., t] = torch.sum(partial_a_all[..., t:n_steps]*grad_delta[..., t:n_steps], dim=4) 
 
         if torch.sum(outputs)/(shape[0] * shape[1] * shape[2] * shape[3] * shape[4]) > 0.1:
-            # part one, inter-neuron
             partial_u = torch.clamp(1 / delta_u, -10, 10) * outputs
             grad = grad_a * partial_u
         else:
